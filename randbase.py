42b31dacd02b2f70993c2ea0e365fb3867988b6f
PhilsCombat <74490270+PhilsCombat@users.noreply.github.com>
2023-03-11 23:35:17 +0100

    Finished argument parser for randbase.py

--- randbase.py
+++ randbase.py
@@ -15,19 +15,81 @@ def randbase(basefunc=int, start:int=0, end:int=1):
     return basefunc(random.randint(start, end))
 
 
-def setDigits(number: str | int, digitCount: int, digit: str = '0', removePrefix = False) -> str:
+def setDigits(number: str | int, digitCount: int, digit: str = "0") -> str:
     if (type(number) == int):
-        number = str(number)
-        return digit * max(0, digitCount - len(number)) + number
-    if (not number[1] in 'box'):
-        raise SyntaxWarning("number MUST contain base-prefix: 0b | 0o | 0x")
+        tmp = str(number)
+        return (max(0, digitCount - len(tmp)) * digit) + tmp
     # add 2 to account for prefix
-    missing = max(0, digitCount - len(number)) + 2
-    if (removePrefix):
-        return digit * missing + number[2:]
+    missing = max(0, digitCount - len(number)) - 2
     return number[:2] + digit * missing + number[2:]
 
+def removePrefix(number: str):
+    if (not number[1] in 'box'):
+        raise SyntaxWarning("number MUST contain base-prefix: 0b | 0o | 0x")
+    return number[2:]
 
+# #main#
 
 if (__name__ == '__main__'):
-    pass
+    import argparse
+    import sys
+    
+    parser = argparse.ArgumentParser(
+        description="Genrate random number as integer, binary, octal, hexadecimal",
+        usage='randbase.py [options] [bin|oct|int|hex]'
+    )
+    parser.add_argument('-s', '--start', type=int, default=0, help='Minimum value')
+    parser.add_argument('-e', '--end', type=int, required=True, help='Maximum value (inclusive)')
+    parser.add_argument('-d', '--digits', type=int, default=0, help='Minimum digit count')
+    parser.add_argument('-l', '--list', action='store_true', help='Make a list of random numbers')  # must be set with --items
+    parser.add_argument('-i', '--items', type=int, default=1, help='Item count of list (default: 1)')  # must be set with --list
+    parser.add_argument('-c', '--clip', action='store_true', help='Put result into clipboard')
+    parser.add_argument('--no-prefix', action='store_true', help='Remove pythons prefix (0x 0o 0b)')
+
+    sub_parser = parser.add_subparsers(dest="base")
+    sub_hex = sub_parser.add_parser('hex')
+    sub_int = sub_parser.add_parser('int')
+    sub_oct = sub_parser.add_parser('oct')
+    sub_bin = sub_parser.add_parser('bin')
+
+    opt = parser.parse_args(sys.argv[1:])
+
+    func=None
+    match(opt.base):
+        case 'hex':
+            func = hex
+        case 'bin':
+            func = bin
+        case 'oct':
+            func = oct
+        case _:
+            func = int
+    
+
+    def generate(): 
+        out = None
+        if (opt.digits > 2):
+            out = setDigits(randbase(func, opt.start, opt.end), opt.digits, '0')
+        else:
+            out = randbase(func, opt.start, opt.end)
+        if (opt.no_prefix):
+            out = removePrefix(out)
+        return out
+
+# =generate=
+
+    val=None
+    if (opt.list):
+        val = []
+        for i in range(opt.items):
+            val.append(generate())
+    else:
+        val = generate()
+
+# =clip=
+
+    if (opt.clip):
+        import pyperclip
+        pyperclip.copy(str(val))
+
+    print(val)
